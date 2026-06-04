caba6c932f46539843721b3b8a526ed0c54fc096
Sagar <32243556+sagarkpatil3@users.noreply.github.com>
2026-06-04 15:44:55 -0700

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.js
@@ -0,0 +1,41 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if(s.length != t.length){
+            return false;
+        }
+
+        let sortedS = s.split('').sort().join('')
+        let sortedT = t.split('').sort().join('')
+        let freqS = {}
+        let freqT = {}
+        let length = sortedS.length;
+        for(let i = 0 ; i < length; i++){
+            if(sortedS[i] in freqS){
+                freqS[sortedS[i]] = freqS[sortedS[i]]+1
+            } else {
+                freqS[sortedS[i]] = 1;
+            }
+
+            if(sortedT[i] in freqT){
+                freqT[sortedT[i]] = freqT[sortedT[i]]+1
+            } else {
+                freqT[sortedT[i]] = 1;
+            }
+        }
+
+        let keysforS = Object.keys(freqS)
+
+        for(let key of keysforS ){
+            if(freqS[key] !== freqT[key]){
+                return false
+            }
+        }
+
+        return true
+    }
+}
